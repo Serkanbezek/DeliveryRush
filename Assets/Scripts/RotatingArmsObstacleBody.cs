d3b7be56565ca583d08bcffd8fb23ac5c1874e87
Serkanbezek <serkanbezek.sb@gmail.com>
2023-02-25 17:40:40 +0300

    Update RotatingArmsObstacleBody.cs

--- Assets/Scripts/RotatingArmsObstacleBody.cs
+++ Assets/Scripts/RotatingArmsObstacleBody.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RotatingArmsObstacleBody : Obstacle
 {
-    
+    [SerializeField] private Vector3 _rotateVector;
+
+    [SerializeField] private float _rotationDuration;
+
+
+    private void Start()
+    {
+        RotateObstacleBody();
+    }
+
+
+    private void RotateObstacleBody()
+    {
+        transform.DORotate(_rotateVector, _rotationDuration).SetLoops(-1).SetEase(Ease.Linear);
+    }
 }
