cc09638071a2d98c4cfd1ac93b75dd1265ea40ad
Serkanbezek <serkanbezek.sb@gmail.com>
2023-03-06 02:30:36 +0300

    Update RoadStep.cs

--- Assets/Scripts/RoadStep.cs
+++ Assets/Scripts/RoadStep.cs
@@ -5,5 +5,25 @@ using DG.Tweening;
 
 public class RoadStep : MonoBehaviour
 {
-   
+    [SerializeField] private Color _roadStepColor;
+
+    private Renderer _roadStepRenderer = null;
+    private MaterialPropertyBlock _materialPropertyBlock = null;
+
+    private void Start()
+    {
+        _roadStepRenderer = GetComponent<Renderer>();
+        _materialPropertyBlock = new MaterialPropertyBlock();
+        _materialPropertyBlock.SetColor("_Color", _roadStepColor);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            _roadStepRenderer.SetPropertyBlock(_materialPropertyBlock);
+        }
+    }
+
 }
