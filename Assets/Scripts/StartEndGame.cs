c3d725a75163fd1d4b2972f15315e644585cf054
Serkanbezek <serkanbezek.sb@gmail.com>
2023-03-08 01:20:25 +0300

    Update StartEndGame.cs

--- Assets/Scripts/StartEndGame.cs
+++ Assets/Scripts/StartEndGame.cs
@@ -5,14 +5,16 @@ using DG.Tweening;
 
 public class StartEndGame : MonoBehaviour
 {
-    [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private bool _isPlayerColliding = false;
     [SerializeField] private TunnelMoveUp _tunnelMoveUp;
-
     [SerializeField] private List<GameObject> _roadSteps = new();
+
+    private ScoreManager _scoreManager;
 
     private void Start()
     {
+        _scoreManager = ScoreManager.Instance;
+
         for (int i = 1; i < _roadSteps.Count; i++)
         {
             Vector3 roadStepPos = _roadSteps[i].transform.localPosition;
@@ -35,10 +37,11 @@ public class StartEndGame : MonoBehaviour
         {
             _isPlayerColliding = false;
             _tunnelMoveUp.MoveTunnelUp();
-            int levelScore = _scoreManager.GetLevelScore();
+            float levelScore = _scoreManager.GetLevelScore();
             float distanceToMove = levelScore / 5f + 4f;
             yield return new WaitForSeconds(1f);
-            other.transform.DOMoveZ(other.transform.position.z + distanceToMove, 4).SetEase(Ease.OutQuart);
+            other.transform.DOMoveZ(other.transform.position.z + distanceToMove, 4).SetEase(Ease.OutQuart).OnComplete(() =>
+            _scoreManager.MultiplyLevelScore());
         }
 
     }
